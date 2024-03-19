df53a3ee53537e0b7bd6c5bce8f2cf41031eee89
spoiuy3 <104378473+spoiuy3@users.noreply.github.com>
2024-03-19 15:22:02 +0900

    OT

---
+++ Source/Game/Main.cpp
@@ -0,0 +1,12 @@
+#include "Common.h"
+
+#include "Game/Game.h"
+#include <stdio.h>
+
+INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ INT nCmdShow)
+{
+
+	library::PrintHi();
+
+	return 0;
+}
---
+++ Source/Library/Game/Game.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Common.h"
+
+namespace library
+{
+    /*--------------------------------------------------------------------
+      Forward declarations
+    --------------------------------------------------------------------*/
+
+    /*F+F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F+++F
+      Function: Render
+
+      Summary:  Render the frame
+    F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F---F-F*/
+    void PrintHi();
+}
